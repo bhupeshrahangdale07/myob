9e5e8045c0ccf4150a0aee0122481670a2ed2d45
Bhupesh Rahangdale <bhupeshrahangdale07@gmail.com>
2025-06-23 17:18:38 +0530

    changes

--- force-app/main/default/lwc/productSync/productSync.js
+++ force-app/main/default/lwc/productSync/productSync.js
@@ -1,32 +1,31 @@
-import { LightningElement, track} from 'lwc';
+import { LightningElement, track,api} from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import LightningConfirm from 'lightning/confirm';
 import { NavigationMixin } from 'lightning/navigation';
 import checkAuthorizationSteps from '@salesforce/apex/MYOB_Callout_Helper.checkAuthorizationSteps';
-import getCustomSettingDetails from '@salesforce/apex/ProductConfigController.getCustomSettingDetails';
 import fetchAllMyobProducts from '@salesforce/apex/ProductDynamicController.fetchAllMyobProducts';
-
+import fetchMYOBObCSConfig from '@salesforce/apex/MYOB_Component_Helper_cls.fetchMYOBObCSConfig';
 
 export default class ProductSync extends NavigationMixin(LightningElement) {
+    @api objectApiName;
+    @api recordId;
     @track isActive = false;
     @track selectedValue = 'All';
     @track isModalOpen = false;
     @track confirmationMessage = '';
 
+    showAllFilter=true;
     showModal= false;
+    showLoading=false;
     title = '';
     body = '';
     showPositiveButton = false;
     showNegativeButton = false;
     positiveButtonLabel = '';
     negativeButtonLabel = '';
+    isProductMapped = false;
 
-    connectedCallback(){
-        
-      
-    }
-
-    // Options for dropdown
+//Getter: Options for dropdown
     get options() {
         return [
             { label: 'All', value: 'All' },
@@ -36,44 +35,115 @@ export default class ProductSync extends NavigationMixin(LightningElement) {
         ];
     }
 
-    // Handle checkbox change
+    connectedCallback() {
+        this.fetchCSConfigs();
+    }
+
+// method : Fetch all the custom settings to get mappings.
+    fetchCSConfigs(){
+        fetchMYOBObCSConfig()
+        .then(response => {
+            if(response){
+                if(response.productObjectApiName && response.mapMYOBApiNameProductData!=null){
+                    this.isProductMapped = true;
+                    if(this.objectApiName && this.recordId){
+                        if(this.objectApiName === response.productObjectApiName){
+                        this.showAllFilter=false;
+                        this.handleSync();
+                        }
+                    }
+                }
+            }
+        }).catch(error => {
+            this.showLoading = false;
+            console.error('!!! Error occurred while getting Custom Setting: ', error);
+            this.showNotification('An unexpected error occurred while retrieving the Custom Setting to check the mappings. Contact System Administrator.','error');
+        })
+    }
+
+//method: Handle active checkbox change
     handleCheckboxChange(event) {
         this.isActive = event.target.checked;
     }
 
-    getCustomSetting(){
-        getCustomSettingDetails()
-        .then(result => {
-            console.log('### getCustomSettingDetails result : ',result);
-            if(result.KTMYOB__Product_Object_Api_Name__c){
-this.confirmation = true;
-}
-        })
-        .catch(error => {
-            this.isLoading = false;
-            console.error('!!! Error occurred while getting Custom Setting: ', error);
-        });
-    } 
-
-    // Handle dropdown change
+//Method: Handle dropdown change
     handleDropdownChange(event) {
         this.selectedValue = event.target.value;
     }
 
-    // Handle Sync Button Click (Opens Confirmation Modal)
-    handleSync() {
-        //this.generateConfirmationMessage();
-        this.showModal = true;
-        this.title = 'Confirm Sync';
-        this.body = this.generateConfirmationMessage();
-        console.log('body value',this.body);
-        this.showPositiveButton = true;
-        this.positiveButtonLabel = 'Confirm';
-        this.showNegativeButton = true;
-        this.negativeButtonLabel = 'Cancel';
+//Method: Check for Connection Steps before processing Sync.
+    getConnectionDetails(){
+        checkAuthorizationSteps()
+        .then(response => {
+            if (response.status === 'Success'){
+                console.log('### Authorization Successful'); 
+                this.fetchAllProductsToSf();
+            } else if (response.status === 'Failed' && response.isConnectionError){
+                console.error('Authorization Failed: ', response.message);
+                this.handleAlert('Authorization Failed: Please complete all connection steps on MYOB Setup Page.','Connection Not Established');
+            }
+        })
+        .catch(error => {
+            this.showLoading = false;
+            this.showNotification('Unexpected error occurred while checking authorization. Contact System Administrator.','error');
+        });
     }
 
-    // Generate Dynamic Confirmation Message
+//method: Fetch all the prdocut from MYOB as per user inputs.
+    fetchAllProductsToSf(){
+        debugger;
+        fetchAllMyobProducts({'syncSingleProduct':false,
+           'isItemActive':this.isActive,
+           'itemType':this.selectedValue})      
+        .then(response => {
+            if (response.status === 'Success') {
+                this.showNotification(response.message,'success');
+            } else if (response.status === 'Failed' || response.isConnectionError) {
+                if(response.message){
+                    this.showNotification(response.message,'error');
+                }else if (response.multipleMessage){
+                    let errrorArr = JSON.parse(response.multipleMessage);
+                    errrorArr.forEach(err => {
+                        let errorMsg = 
+                        'Name      : ' + err.Name + ' | ' +
+                        'Message   : ' + err.Message + ' | ' +
+                        'ErrorCode : ' + err.ErrorCode;
+                        if(err.Severity.toLowerCase() === 'error'){
+                            this.showNotification(errorMsg,'error','sticky');
+                        }else if((err.Severity.toLowerCase() === 'warning')){
+                            this.showNotification(errorMsg,'warning','sticky');
+                        }
+                    });
+                }else{
+                    this.showNotification('Unexpected Error : Contact your System Administrator,','error');
+                } 
+            }
+            this.showLoading = false;
+        }).catch(error => {
+            this.showLoading = false;
+            this.showNotification('Product Sync from Salesfore to MYOB Failed. Contact System Administrator.','error');
+        });
+    }
+
+    
+
+//method: Handle Sync Button Click (Opens Confirmation Modal)
+    handleSync() {
+        if(this.isProductMapped){
+            this.showModal = true;
+            this.title = 'Confirm Sync';
+            this.body = this.generateConfirmationMessage();
+            console.log('body value',this.body);
+            this.showPositiveButton = true;
+            this.positiveButtonLabel = 'Confirm';
+            this.showNegativeButton = true;
+            this.negativeButtonLabel = 'Cancel';
+        }else {
+            this.showNotification('Please ensure that the product and its required fields are mapped before proceeding with the sync.','error');
+        }
+    }
+
+//helper: Generate Dynamic Confirmation Message
     generateConfirmationMessage() {
         let action = '';
         if (this.selectedValue === 'IsSold') {
@@ -93,56 +163,20 @@ this.confirmation = true;
         }
     }
 
-    // Close Modal
+//helper: Close Modal
     closeModal() {
         this.showModal = false;
     }
 
-    // Confirm Sync (Trigger Actual Sync Logic)
-    confirmSync() {
+//helper: Confirm Sync (Trigger Actual Sync Logic)
+    confirmSync(event) {
+        const data = event.detail;
+        this.showLoading = true;
         this.showModal = false;
-        console.log('Syncing Data to Salesforce...');
-        console.log(`Active: ${this.isActive}, Selected Option: ${this.selectedValue}`);
-
-        // 01 Pooja checkAuthorizationSteps() : on sucess do callout , failure navigate to Setuppage
-
-          this.getConnectionDetails();
-
-        //Calling the method from ProductDynamicController
-       
-
-
+        this.getConnectionDetails();
     }
 
-     getConnectionDetails(){
-        checkAuthorizationSteps()
-        .then(response => {
-            if (response.status === 'Success') {
-                console.log('### Authorization Successful');
-                // Mangesh to Review
-                fetchAllMyobProducts('',false,this.isActive,this.selectedValue)
-        .then(result => {
-            console.log('Fetch successful:', result);
-            // Handle success - maybe process the result
-            // 02 Pooja  : based on ComponentResponse class  , based on status show toast message
-        })
-        .catch(error => {
-            console.error('Error fetching products:', error);
-            // Handle error - maybe show an error message
-        });
-
-            } else if (response.status === 'Failed' && response.isConnectionError) {
-                console.error('Authorization Failed: ', response.message);
-                //this.showNotification('Authorization Failed: Please complete all connection steps on MYOB Setup Page.','error');
-                this.handleAlert('Authorization Failed: Please complete all connection steps on MYOB Setup Page.','Connection Not Established');
-            }
-        })
-        .catch(error => {
-            this.showLoading = false;
-            console.error('!!! Error occurred while checking authorization: ', error);
-        });
-    }
-
+//helper : to show alert when th authorizaton steps not completed.
     handleAlert(msg, lbl) {
         LightningConfirm.open({
             label: lbl,
@@ -170,30 +204,26 @@ this.confirmation = true;
         });
     }
 
-     backBtnHandler(){
-        const pageReference = {
-            attributes: {
-                actionName: 'view',
-                objectApiName: this.objectApiName,
-                recordId: this.recordId
-            },
-            type: 'standard__recordPage',
-        };
-        this[NavigationMixin.Navigate](pageReference);
-    }
+//helper : to show toast messag.    
+    showNotification(msg,type) {
+        if (typeof window !== 'undefined') {
+            const evt = new ShowToastEvent({
+                title:'Product Sync',
+                message: msg,
+                variant: type
+            });
+            this.dispatchEvent(evt);
 
-     showNotification(msg,type) {
-    
-            if (typeof window !== 'undefined') {
-    
-                const evt = new ShowToastEvent({
-                    title:'Invoice Generation',
-                    message: msg,
-                    variant: type
-                });
-                this.dispatchEvent(evt);
-    
-            }
         }
-
+    }
 }
+
+
+
+
+//TODO : 
+// 1. Fix the apex method products getting sync succesfully, but Logsnot getting generated.
+// 2. Fix the code and add proper loading and imporve UX.
+// 3. Fix th ConfirmationModalPopup child component.
+// 4. Review ProductDynamicController class.
+// 4. Fix the desiging at the end.
